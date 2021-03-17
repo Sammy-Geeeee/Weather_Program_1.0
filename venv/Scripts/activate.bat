8f49fe90573ef29b14c6f028cb67f3c1e3adfbd0
Sam <Sam.Givan@outlook.com>
2021-03-17 15:50:23 +1300

    File reconfigurations.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documents\# Programs\Dropbox\Dropbox\1 - Personal\Programming\Projects\Weather_Program\Weather_Program_1.0\venv
+set VIRTUAL_ENV=D:\GoogleDrive\1 - Personal\Programming\Projects\Weather_Program\Weather_Program_1.0\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
